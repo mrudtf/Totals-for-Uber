21a67f43fcdf7738ffe9f0e086cb7db91e19e968
Bryce Adams <brycead@gmail.com>
2015-08-14 18:05:22 +0700

    No longer own, so not advertising it.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,5 @@
 <div class="row">
     <div class="col-md-12 footer">
-        <div class="other-thing">
-            I built this other thing, <a href="https://nomadsms.com?ref=ubertotals"><i class="fa fa-comment"></i> Nomad SMS</a> - Check it out!
-        </div>
         Made with <i class="fa fa-heart"></i> in Seoul by <img src="https://pbs.twimg.com/profile_images/552662851916021760/otDgtZJ0.jpeg" width="30" class="bryce" /> <a href="http://twitter.com/bryceadams/" class="brycese">bryce</a>
         <span class="built-hackathon">Built for the <a href="http://uberhackathon.challengepost.com/">2015 Uber API Hackathon</a></span>
         <div class="links">
